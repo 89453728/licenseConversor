b558d94dd1a9ec55e924160a09c261c427db39a1
89453728 <gooogle.notifications.sec@gmail.com>
2022-01-05 22:12:21 +0100

    license header at src/fformat.m

--- src/fformat.m
+++ src/fformat.m
@@ -1,3 +1,20 @@
+## fformat.m
+## Copyright (C) 2022-2023  Yassin Achengli <0619883460@uma.es>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 function r = fformat(file_list)
         ## All the files must be writted in csv format with the name and the license to be converted 
         ## in a list of structs, those structs must be with the next template
